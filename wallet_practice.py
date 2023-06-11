e32e91174d2f39593bc78a17841e2a05528a6407
Ben | Monarch <bgoober2@protonmail.com>
2023-06-11 04:57:28 -0400

    add queries to wallet practice

--- wallet_practice.py
+++ wallet_practice.py
@@ -1,5 +1,6 @@
 from cosmpy.aerial.wallet import LocalWallet
 from cosmpy.crypto.keypairs import PrivateKey
+from cosmpy.aerial.client import LedgerClient, NetworkConfig
 
 # To create a random private key:
 # private_key = PrivateKey()
@@ -14,3 +15,18 @@ address = wallet.address()
 print(address)
 
 # this generates a new private key, wallet, and address every time since no mnemonic is saved
+
+# define the blockchain network to connect to, in this case fetch mainnet
+ledger = LedgerClient(NetworkConfig.fetch_mainnet())
+
+# query all balances for the address
+balances = ledger.query_bank_all_balances(address)
+print(f"balances are: {balances}")
+
+# query just the afet balance for the address
+balance = ledger.query_bank_balance(address, denom='afet')
+print(f"afet balance is: {balance}")
+
+# staking summary for address
+staking_summary = ledger.query_staking_summary(address)
+print(staking_summary)
